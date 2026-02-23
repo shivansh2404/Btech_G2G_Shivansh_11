92f176c8ff2f7dbb17360235d81e075a08ec5ea9
shivansh2404 <shivansh211564@gmail.com>
2026-02-23 11:32:28 +0530

    Add files via upload

---
+++ Shivansh vowel or constant.c.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    ch = tolower(ch);
+
+    if ((ch >= 'a' && ch <= 'z')) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            printf("'%c' is a Vowel.\n", ch);
+        } else {
+            printf("'%c' is a Consonant.\n", ch);
+        }
+    } else {
+        printf("'%c' is not an alphabet character.\n", ch);
+    }
+
+    return 0;
+}
